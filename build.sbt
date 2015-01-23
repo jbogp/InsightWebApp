8e6e901114f6bb1464152712cb667dd5ce8a5a92
jb pettit <jbpettit@ebi.ac.uk>
2015-01-23 19:11:33 +0000

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,39 @@
+name := "InsightWebApp"
+
+organization  := "com.example"
+
+version       := "0.1"
+
+scalaVersion  := "2.11.2"
+
+resolvers ++= Seq(
+  "Hadoop Releases" at "https://repository.cloudera.com/content/repositories/releases/",
+  "Cloudera" at "https://repository.cloudera.com/artifactory/public/",
+  "Cloudera2" at "http://repository.cloudera.com/cloudera/cloudera-repos/",
+  "releases" at "http://oss.sonatype.org/content/repositories/releases"
+)
+
+scalacOptions := Seq("-unchecked", "-deprecation", "-encoding", "utf8")
+
+libraryDependencies ++= {
+  val akkaV = "2.3.6"
+  val sprayV = "1.3.2"
+  Seq(
+    "io.spray"            %%  "spray-can"     % sprayV,
+    "io.spray"            %%  "spray-routing" % sprayV,
+    "io.spray"            %%  "spray-testkit" % sprayV  % "test",
+    "com.typesafe.akka"   %%  "akka-actor"    % akkaV,
+    "com.typesafe.akka"   %%  "akka-testkit"  % akkaV   % "test",
+    "org.specs2"          %%  "specs2-core"   % "2.3.11" % "test",
+    "org.apache.hbase" % "hbase-client" % "0.98.6-cdh5.3.0",
+    "org.apache.hbase" % "hbase-protocol" % "0.98.6-cdh5.3.0",
+    "org.apache.hbase" % "hbase-common" % "0.98.6-cdh5.3.0",
+    "org.apache.hbase" % "hbase-server" % "0.98.6-cdh5.3.0",
+    "org.apache.hadoop" % "hadoop-core" % "2.5.0-mr1-cdh5.3.0",
+    "org.apache.hadoop" % "hadoop-common" % "2.5.0-cdh5.3.0",
+    "org.apache.hadoop" % "hadoop-client" % "2.5.0-mr1-cdh5.3.0",
+    "net.liftweb" % "lift-json-ext_2.10" % "2.5.1"
+  )
+}
+
+Revolver.settings
---
+++ src/main/scala/com/example/ReadFromHbase.scala
@@ -0,0 +1,63 @@
+package main.scala.hbase
+
+import org.apache.hadoop.hbase.client.HTable
+import org.apache.hadoop.hbase.client.HBaseAdmin
+import org.apache.hadoop.hbase.client.Get
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.hbase.util.Bytes
+import java.util.Calendar
+import org.apache.hadoop.hbase.client.Scan
+import scala.collection.mutable.ArrayBuffer
+import net.liftweb.json.DefaultFormats
+import net.liftweb.json._
+import org.apache.hadoop.hbase.client.Result
+
+
+class ReadFromHbase {
+  
+  	/*Creating configuration and connecting*/
+	val conf = new Configuration()
+	val admin = new HBaseAdmin(conf)
+	implicit val formats = Serialization.formats(NoTypeHints)
+	
+	/*Generic Hbase reader to fetch all the rows of a table beetween 2 times and create objects out of that*/
+	def readTimeFilterGeneric[T](table:String,minutesBackMax:Int,minutesBackMin:Int,handleRow:Result=>T):ArrayBuffer[T] = {
+		/*Fetch the table*/
+		val httable = new HTable(conf, table)
+		val offsetMax:Long = minutesBackMax*60000L
+		val offsetMin:Long = minutesBackMin*60000L
+		val theScan = new Scan().setTimeRange(Calendar.getInstance().getTimeInMillis()-offsetMax, Calendar.getInstance().getTimeInMillis()-offsetMin);
+		
+		/*Adding timestamp filter*/
+		val res = httable.getScanner(theScan)
+
+		val iterator = res.iterator()
+		val ret = new ArrayBuffer[T]
+		while(iterator.hasNext()) {
+			val next = iterator.next()
+			ret.append(handleRow(next))		
+		}
+		ret		
+	}
+	
+	case class Comment(created_at:String,from:String,like_count:Int,message:String)
+ 
+	
+	def readTimeFilterComments(table:String,column:String,minutesBackMax:Int,minutesBackMin:Int):ArrayBuffer[Comment] =  {
+		/*function to handle meta link results*/
+		def handleRow(next:Result):Comment = {
+			val jsonString = {
+			  val col = next.getColumn("infos".getBytes(), column.getBytes())
+			  if(col.isEmpty())
+			    ""
+			  else
+			     new String(col.get(0).getValue())
+			}
+			val json = parse(jsonString)
+			json.extract[Comment]
+		}
+		/*Calling the database*/
+		readTimeFilterGeneric[Comment](table, minutesBackMax, minutesBackMin, handleRow)
+	}
+
+}

8ac78f01fdf922eb47437ebb755f9f9e63c9a8b1
jb pettit <jbpettit@ebi.ac.uk>
2015-01-26 07:41:43 +0000

    Getting comments links

--- src/main/scala/com/example/GetCommentsTopic.scala
+++ src/main/scala/com/example/GetCommentsTopic.scala
@@ -4,15 +4,9 @@ import scala.collection.mutable.ArrayBuffer
 
 object GetCommentsTopic {
   
-  	def getCommentsUlrs(value:ArrayBuffer[ListOfComments]):String = {
-		val ret = value.map(_.url)
-			.mkString("<hr>")
-			println(ret)
-			ret
-	}
 
-	def getCommentsHTML(value:ArrayBuffer[ListOfComments]):String = {
-		value.map(_.comments)
+	def getCommentsHTML(value:ArrayBuffer[List[Comment]]):String = {
+		value
 			.reduce((t,i)=> t:::i)
 			.sortBy(- _.like_count)
 			.map(t=>(t.created_time+" "+t.from+"("+t.like_count+"): "+t.message))

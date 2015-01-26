db19cadb8746117b2696b4d426a356f0c87ccc48
jb pettit <jbpettit@ebi.ac.uk>
2015-01-26 20:21:35 +0000

    Setting new path

--- src/main/scala/com/example/GetCommentsTopic.scala
+++ src/main/scala/com/example/GetCommentsTopic.scala
@@ -9,7 +9,10 @@ object GetCommentsTopic {
 		value
 			.reduce((t,i)=> t:::i)
 			.sortBy(- _.like_count)
-			.map(t=>(t.created_time+" "+t.from+"("+t.like_count+"): "+t.message+" "+t.url))
+			.map(t=>t.url match {
+				case Some(_) => t.created_time+" "+t.from+"("+t.like_count+"): "+t.message+" "+t.url.get
+				case _ => t.created_time+" "+t.from+"("+t.like_count+"): "+t.message
+			})
 			.mkString("<hr>")
 	}
   

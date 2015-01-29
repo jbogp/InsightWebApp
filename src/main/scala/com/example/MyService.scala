3e85089100c03de34bca5053019f29b0eb951662
jb pettit <jbpettit@ebi.ac.uk>
2015-01-29 19:21:20 +0000

    Tweets API

--- src/main/scala/com/example/MyService.scala
+++ src/main/scala/com/example/MyService.scala
@@ -73,7 +73,7 @@ Nothing to see here
 		}~
 		path("tweets"){
 			parameters('req) { (req) =>
-			    onComplete(test.readFutureTimeFilterTweets("commentsalltime", req, 600, 0)) {
+			    onComplete(test.readFutureTimeFilterTweets("commentsalltime", req, 6000, 0)) {
 			    	      case Success(value) => respondWithMediaType(`application/json`) {
 			    	        complete{
 			    	        	GetCommentsTopic.getTweetsJson(value)

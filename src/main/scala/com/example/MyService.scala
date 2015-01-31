c8c6b429418576b89466f39d7bfe7555e3a8dfd5
jb pettit <jbpettit@ebi.ac.uk>
2015-01-31 07:35:44 +0000

    ordering by date the comments

--- src/main/scala/com/example/MyService.scala
+++ src/main/scala/com/example/MyService.scala
@@ -79,7 +79,7 @@ Nothing to see here
 		}~
 		path("tweets"){
 			parameters('req) { (req) => respondWithHeader(RawHeader("Access-Control-Allow-Origin", "*")){
-			    onComplete(ReadFromHbase.readFutureTimeFilterTweets("commentsalltime", "theTweets_"+req, 60, 0)) {
+			    onComplete(ReadFromHbase.readFutureTimeFilterTweets("commentsalltime", "theTweets_"+req, 600, 0)) {
 			    	      case Success(value) => respondWithMediaType(`application/json`) {
 			    	        complete{
 			    	        	GetCommentsTopic.getTweetsJson(value)
